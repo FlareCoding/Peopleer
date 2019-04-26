45953255f26aa58ba2b4173781c73e42358174f6
FlareCoding <albert.slepak@gmail.com>
2019-04-25 21:08:31 -0400

    removed android client project

--- Peopleer/iOS/PeopleerClient/PeopleerClient/EventDataManager.swift
+++ Peopleer/iOS/PeopleerClient/PeopleerClient/EventDataManager.swift
@@ -12,8 +12,8 @@ class EventDataManager {
     static var event_locations = [
         ["type": "event", "title": "Peopleer Headquarters", "latitude": 40.711017, "longitude": -74.016937],
         ["type": "event", "title": "Test Event 1", "latitude": 40.701983, "longitude": -74.014356],
-        ["type": "event", "title": "Test Event 2", "latitude": 40.709313, "longitude": -74.003489],
-        ["type": "event", "title": "Test Event 3", "latitude": 40.692753, "longitude": -73.976649]
+        ["type": "event", "title": "Test Event 2", "latitude": 40.709313, "longitude": -74.003489]
+        //["type": "event", "title": "Test Event 3", "latitude": 40.692753, "longitude": -73.976649]
     ]
     
     static func RefreshEventData() {
